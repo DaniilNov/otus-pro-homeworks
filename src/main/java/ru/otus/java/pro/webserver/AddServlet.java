738b3e87e9326ea033cd811048a08fc6d954ae22
xa86 <xa86@mail.ru>
2025-01-22 11:09:38 +0300

    Homework Web Server v2

--- src/main/java/ru/otus/java/pro/webserver/AddServlet.java
+++ src/main/java/ru/otus/java/pro/webserver/AddServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/online-calculator/add")
+@WebServlet("/add")
 public class AddServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
--- src/main/java/ru/otus/java/pro/webserver/SubtractServlet.java
+++ src/main/java/ru/otus/java/pro/webserver/SubtractServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/online-calculator/subtract")
+@WebServlet("/subtract")
 public class SubtractServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
